b9af7b8517cc168286b39c8a67b72a65dc536804
AGBarbour <alex.g.barbour>
2018-10-21 13:25:41 -0500

    Finished pie chart and fixed reveal/leaflet display issue

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
 // Map variables
-var mapZoom = 13
-var mapCenter = [37.7749, -122.4194]
+var mapZoom = 2
+var mapCenter = [20, 0]
 
 // Create Map
 var myMap = L.map("map", {
@@ -15,6 +15,23 @@ id: "mapbox.streets",
 accessToken: API_KEY
 }).addTo(myMap);
 
+
+L.Map.addInitHook(function() {
+    var slides = document.querySelector('.slides'),
+        zoom = Number(slides.style.zoom);
+
+    // Reveal.js sometimes use the zoom CSS property, but sometimes a scale
+    // transform instead. We handle both.
+    if (zoom) {
+        this._container.style.zoom = 1/zoom;
+    } else {
+        zoom = Number(slides.style.transform.replace(/.*scale\(([0-9\.]+)\).*/, '$1'));
+        this._container.style.transform = 'scale(' + (1 / zoom) + ')';
+    }
+
+    this.invalidateSize();
+});
+
 // url will be set to restAPI when that is set up using python
 var url = "";
 
@@ -23,3 +40,10 @@ d3.json(url, function(response) {
     // Call data and .addTo(myMap)
 
 });
+
+Reveal.addEventListener( 'slidechanged', function( event ) {
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+    if (event.currentSlide){ // assuming your 5th slide is the one with the map
+        myMap.invalidateSize(false); // assuming that map holds the the reference to your leaflet instance
+    }
+} );
